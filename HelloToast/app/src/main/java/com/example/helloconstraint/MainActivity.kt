fe0af90777497eb84aa28443220ece6b264f7321
Dmitry <darksign@list.ru>
2022-01-12 19:27:50 +0300

    HelloConstraint HW

---
+++ HelloToast/app/src/main/java/com/example/helloconstraint/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.helloconstraint
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.content.res.AppCompatResources
+
+class MainActivity : AppCompatActivity() {
+    private var mCount = 0
+    lateinit var mShowCount: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        mShowCount = findViewById(R.id.show_count)
+    }
+
+    fun showToast(view: android.view.View) {
+        Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT).show()
+    }
+
+    fun countUp(view: android.view.View) {
+        mCount++
+        if (mCount % 2 == 0) {
+            view.backgroundTintList = AppCompatResources.getColorStateList(this, R.color.color_even);
+        } else {
+            view.backgroundTintList = AppCompatResources.getColorStateList(this, R.color.color_odd);
+        }
+        findViewById<Button>(R.id.button_zero).backgroundTintList = AppCompatResources.getColorStateList(this, R.color.color_even)
+        mShowCount.text = mCount.toString()
+    }
+
+    fun resetCount(view: android.view.View) {
+        mCount = 0
+        view.backgroundTintList = AppCompatResources.getColorStateList(this, R.color.color_gray)
+        mShowCount.text = mCount.toString()
+    }
+}

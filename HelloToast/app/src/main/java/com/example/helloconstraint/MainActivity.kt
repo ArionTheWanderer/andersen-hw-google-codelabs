a2a5e0d6d8019e35f22bd6d6435ccd7a00683c58
Dmitry <darksign@list.ru>
2022-01-12 20:57:58 +0300

    [2]HelloConstraint HW: linear layout has been replaced by constraint layout; some little fixes

--- HelloToast/app/src/main/java/com/example/helloconstraint/MainActivity.kt
+++ HelloToast/app/src/main/java/com/example/helloconstraint/MainActivity.kt
@@ -9,6 +9,11 @@ import androidx.appcompat.content.res.AppCompatResources
 
 class MainActivity : AppCompatActivity() {
     private var mCount = 0
+        set(value) {
+            field = value
+            mShowCount.text = value.toString()
+        }
+
     lateinit var mShowCount: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,17 +29,16 @@ class MainActivity : AppCompatActivity() {
     fun countUp(view: android.view.View) {
         mCount++
         if (mCount % 2 == 0) {
-            view.backgroundTintList = AppCompatResources.getColorStateList(this, R.color.color_even);
+            view.backgroundTintList = AppCompatResources.getColorStateList(this, R.color.color_even)
         } else {
-            view.backgroundTintList = AppCompatResources.getColorStateList(this, R.color.color_odd);
+            view.backgroundTintList = AppCompatResources.getColorStateList(this, R.color.color_odd)
         }
-        findViewById<Button>(R.id.button_zero).backgroundTintList = AppCompatResources.getColorStateList(this, R.color.color_even)
-        mShowCount.text = mCount.toString()
+        findViewById<Button>(R.id.button_zero).backgroundTintList = AppCompatResources.getColorStateList(this, R.color.color_zero_active)
     }
 
     fun resetCount(view: android.view.View) {
         mCount = 0
-        view.backgroundTintList = AppCompatResources.getColorStateList(this, R.color.color_gray)
-        mShowCount.text = mCount.toString()
+        findViewById<Button>(R.id.button_count).backgroundTintList = AppCompatResources.getColorStateList(this, R.color.color_even)
+        view.backgroundTintList = AppCompatResources.getColorStateList(this, R.color.color_zero_inactive)
     }
 }

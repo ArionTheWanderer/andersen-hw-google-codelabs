6b222f95c8d037697a27fd32f5164c8bfa43bb46
Dmitry <darksign@list.ru>
2022-01-14 17:29:59 +0300

    ImplicitIntents

---
+++ ImplicitIntents/app/src/main/java/com/example/implicitintents/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.implicitintents
+
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import android.content.Intent
+import android.util.Log
+import android.widget.Button
+import androidx.core.app.ShareCompat
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mWebsiteEditText: EditText
+    private lateinit var mLocationEditText: EditText
+    private lateinit var mShareTextEditText: EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        mWebsiteEditText = findViewById(R.id.website_edittext)
+        mLocationEditText = findViewById(R.id.location_edittext)
+        mShareTextEditText = findViewById(R.id.share_edittext)
+
+        findViewById<Button>(R.id.open_website_button).setOnClickListener {
+            val url = mWebsiteEditText.text.toString()
+            val webpage = Uri.parse(url)
+            val intent = Intent(Intent.ACTION_VIEW, webpage)
+            if (intent.resolveActivity(packageManager) != null) {
+                startActivity(intent)
+            } else {
+                Log.d("ImplicitIntents", "Can't handle this!")
+            }
+        }
+
+        findViewById<Button>(R.id.open_location_button).setOnClickListener {
+            val loc = mLocationEditText.text.toString()
+            val addressUri = Uri.parse("geo:0,0?q=$loc")
+            val intent = Intent(Intent.ACTION_VIEW, addressUri)
+            if (intent.resolveActivity(packageManager) != null) {
+                startActivity(intent);
+            } else {
+                Log.d("ImplicitIntents", "Can't handle this intent!");
+            }
+        }
+
+        findViewById<Button>(R.id.share_text_button).setOnClickListener {
+            val txt = mShareTextEditText.text.toString()
+            val mimeType = "text/plain"
+            ShareCompat.IntentBuilder(this)
+                .setType(mimeType)
+                .setChooserTitle(getString(R.string.share_text_with))
+                .setText(txt)
+                .startChooser()
+        }
+    }
+}
